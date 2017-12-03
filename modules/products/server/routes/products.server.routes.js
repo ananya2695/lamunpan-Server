123baf0d111b36ae9a13d232ff7fdcfdc6b77cf3
nicknameismos <mynameissarawut@gmail.com>
2017-12-03 12:34:30 +0700

    update

--- modules/products/server/routes/products.server.routes.js
+++ modules/products/server/routes/products.server.routes.js
@@ -20,6 +20,9 @@ module.exports = function (app) {
   app.route('/api/productsbycategories') //.all(productsPolicy.isAllowed)
     .get(products.getCategory, products.getProducts, products.cookingBestseller);
 
+
+  app.route('/api/productsbycate/:productbycateId') //.all(core.requiresLoginToken, productsPolicy.isAllowed)
+    .get(products.cookingProductList, products.productByCate);
   // Finish by binding the Product middleware
   app.param('productId', products.productByID);
   app.param('productbycateId', products.cateID);
